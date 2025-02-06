6d516e62565cd4800085df0da08f7a8a488b4879
Park YongJune <redplus@redplus.net>
2025-02-06 13:45:06 +0900

    부호 없는 오른쪽 시프트 연산자(>>>) 사용하기

---
+++ textbook/UnsignedRightShiftDemo/UnsignedRightShiftDemo/UnsignedRightShiftDemo.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+class UnsignedRightShiftDemo
+{
+    static void Main()
+    {
+        int x = -4;
+        WriteLine($"{x,11}, {Convert.ToString(x, 2).PadLeft(32, '0'),32}");
+
+        int y = x >> 1;
+        WriteLine($"{y,11}, {Convert.ToString(y, 2).PadLeft(32, '0'),32}");
+
+        int z = x >>> 1;
+        WriteLine($"{z,11}, {Convert.ToString(z, 2).PadLeft(32, '0'),32}");
+    }
+}
