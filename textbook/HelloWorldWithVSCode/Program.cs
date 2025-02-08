10203633c034a5c7636b01a20657932583466523
Park YongJune <redplus@redplus.net>
2025-02-09 08:14:14 +0900

    VScode HelloWorld with C# Dev Kit

---
+++ textbook/HelloWorldWithVSCode/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, C#!");
+        Console.WriteLine("Hello, C# Dev Kit!");
+        System.Console.WriteLine("Hello, VSCode!");
+    }
+}
