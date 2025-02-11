692c89ff5e88c65d9cb14204782104e56074aaa1
Park YongJune <redplus@redplus.net>
2025-02-11 11:46:12 +0900

    ConsoleColor 열거형으로 콘솔의 전경색 및 배경색 바꾸기

---
+++ textbook/ConsoleColorDemo/ConsoleColorDemo/ConsoleColorDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class ConsoleColorDemo
+{
+    static void Main()
+    {
+        // 전경색 변경
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("Blue");
+        Console.ResetColor();
+
+        // 배경색 변경
+        Console.BackgroundColor = ConsoleColor.Yellow;
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Red");
+        Console.ResetColor();
+    }
+}
