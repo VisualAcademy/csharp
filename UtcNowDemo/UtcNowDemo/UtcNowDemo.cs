7511b04aee2d9a28604661e031b884f548e60459
Park YongJune <redplus@redplus.net>
2025-02-18 22:41:49 +0900

    UtcNowDemo

---
+++ UtcNowDemo/UtcNowDemo/UtcNowDemo.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+class UtcNowDemo
+{
+    static void Main()
+    {
+        WriteLine("로컬 시간: " + DateTime.Now);
+        WriteLine("UTC 시간: " + DateTime.UtcNow);
+        WriteLine("UTC를 로컬 시간으로 변환: " + DateTime.UtcNow.ToLocalTime());
+
+        WriteLine("UTC 시간: " + DateTimeOffset.UtcNow);
+        WriteLine("UTC를 로컬 시간으로 변환: " + DateTimeOffset.UtcNow.ToLocalTime());
+
+        WriteLine("로컬 시간: " + DateTimeOffset.Now.DateTime);
+    }
+}
