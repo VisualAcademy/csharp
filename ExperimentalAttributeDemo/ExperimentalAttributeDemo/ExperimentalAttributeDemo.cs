99a99700f18e5db018bcb40b5be3d469bae16e19
Park YongJune <redplus@redplus.net>
2025-02-19 19:43:36 +0900

    ExperimentalAttributeDemo

---
+++ ExperimentalAttributeDemo/ExperimentalAttributeDemo/ExperimentalAttributeDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+public class ExperimentalAttributeDemo
+{
+    [Experimental("ExperimentalFeature")]
+    static void ExperimentalMethod()
+    {
+        Console.WriteLine("이것은 실험적인 기능입니다.");
+    }
+
+    static void Main()
+    {
+#pragma warning disable ExperimentalFeature
+        ExperimentalMethod();
+#pragma warning restore ExperimentalFeature
+    }
+}
