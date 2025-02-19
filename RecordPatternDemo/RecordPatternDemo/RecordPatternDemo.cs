0f216edb202f656f8320342594d9910cba380280
Park YongJune <redplus@redplus.net>
2025-02-20 06:21:57 +0900

    RecordPatternDemo

---
+++ RecordPatternDemo/RecordPatternDemo/RecordPatternDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public record Person(string Name, int Age);
+
+class RecordPatternDemo
+{
+    static void Main()
+    {
+        Person person = new("홍길동", 21);
+
+        string description = person switch
+        {
+            { Name: "홍길동", Age: 21 } => "홍길동은(는) 21세입니다.",
+            { Age: > 18 } => "성인입니다.",
+            _ => "미성년자입니다."
+        };
+
+        Console.WriteLine(description);
+    }
+}
