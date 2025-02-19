4fbdc22f010c9f9f8f43a8288508f4e48bf45478
Park YongJune <redplus@redplus.net>
2025-02-19 20:58:36 +0900

    Span<T>와 ReadOnlySpan<T> 사용하기

---
+++ ReadOnlySpanDemo/ReadOnlySpanDemo/ReadOnlySpanDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ReadOnlySpanDemo
+{
+    static void Main()
+    {
+        string text = "Hello, Span!";
+
+        // 문자열을 ReadOnlySpan으로 감싸기
+        ReadOnlySpan<char> readOnlySpan = text.AsSpan();
+
+        Console.WriteLine(readOnlySpan[0]); // H
+
+        // readOnlySpan[0] = 'X'; // 컴파일 에러 (읽기 전용)
+    }
+}
---
+++ SpanDemo/SpanDemo/SpanDemo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class SpanDemo
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        Span<int> span = numbers; // 배열을 Span으로 감싸기
+
+        span[0] = 10; // 원본 배열도 변경됨
+        Console.WriteLine(string.Join(", ", numbers)); // 10, 2, 3, 4, 5
+    }
+}
