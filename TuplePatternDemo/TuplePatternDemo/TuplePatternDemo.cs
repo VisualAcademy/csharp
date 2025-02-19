4a67ce2ab7a5b439ffbdc427aa6df0bb9a2e50e2
Park YongJune <redplus@redplus.net>
2025-02-20 06:14:56 +0900

    TuplePatternDemo

---
+++ TuplePatternDemo/TuplePatternDemo/TuplePatternDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class TuplePatternDemo
+{
+    static void Main()
+    {
+        (int, int) point = (0, 5);
+        string position = point switch
+        {
+            (0, 0) => "원점",
+            (0, _) => "Y축 위",
+            (_, 0) => "X축 위",
+            _ => "일반 위치"
+        };
+
+        Console.WriteLine($"({point.Item1}, {point.Item2}) -> {position}");
+    }
+}
