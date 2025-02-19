07abb7a30bb35819bfc7389110a553a17a56d141
Park YongJune <redplus@redplus.net>
2025-02-20 06:56:33 +0900

    Record

---
+++ RecordDemo/RecordDemo/RecordDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+// 구독자 정보를 나타내는 레코드
+record Subscriber(string Title, int Duration, bool IsAvailable);
+
+class RecordDemo
+{
+    static void Main()
+    {
+        // 구독자 개체 생성
+        var subscriber = new Subscriber("Visual", 3, true);
+
+        // 새로운 제목을 가진 개체 생성
+        var vip = subscriber with { Title = "VIP" };
+
+        Console.WriteLine(vip);
+
+        // 개체 값 분해
+        var (title, duration, isAvailable) = subscriber;
+
+        Console.WriteLine($"{title} - {duration} - {isAvailable}");
+    }
+}
---
+++ RecordStructDemo/RecordStructDemo/RecordStructDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// 직원 정보를 나타내는 레코드 클래스
+public record class EmployeeClass(string Name, int Age);
+
+// 직원 정보를 나타내는 레코드 구조체
+public record struct EmployeeStruct(string Name, int Age);
+
+class RecordStructDemo
+{
+    static void Main()
+    {
+        // 직원 개체 생성
+        var employee1 = new EmployeeClass("홍길동", 21);
+
+        // 새로운 나이를 가진 개체 생성
+        var employee2 = employee1 with { Age = 31 };
+
+        Console.WriteLine(employee2);
+    }
+}
