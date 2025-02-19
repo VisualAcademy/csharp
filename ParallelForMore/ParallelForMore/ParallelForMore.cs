ad95953df0058c5d7cf51c886ab7f273bbe9fb34
Park YongJune <redplus@redplus.net>
2025-02-20 04:14:06 +0900

    .

--- ParallelForMore/ParallelForMore/ParallelForMore.cs
+++ ParallelForMore/ParallelForMore/ParallelForMore.cs
@@ -1,19 +1,14 @@
-﻿class ParallelForMore
+﻿Parallel.For(0, 200_000, (i) =>
 {
-    static void Main()
+    double result = 0;
+    for (int j = 0; j < 100_000; j++) // 연산을 추가하여 CPU 부하 증가
     {
-        Parallel.For(0, 200_000, (i) =>
-        {
-            double result = 0;
-            for (int j = 0; j < 100_000; j++) // 연산을 추가하여 CPU 부하 증가
-            {
-                result += Math.Sqrt(i) * Math.Pow(i, 2);
-            }
-
-            if (i % 50_000 == 0)
-            {
-                Console.WriteLine($"i: {i}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
-            }
-        });
+        result += Math.Sqrt(i) * Math.Pow(i, 2);
     }
-}
+
+    if (i % 50_000 == 0)
+    {
+        Console.WriteLine(
+            $"i: {i}, Thread ID: {Thread.CurrentThread.ManagedThreadId}");
+    }
+});
