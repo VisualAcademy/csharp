b55697048e44f2017b91008095b6e583fc9dbe01
Park YongJune <redplus@redplus.net>
2025-02-20 06:17:02 +0900

    ListPatternDemo

---
+++ ListPatternDemo/ListPatternDemo/ListPatternDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class ListPatternDemo
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3 };
+
+        // 리스트 패턴을 활용한 비교
+        if (numbers is [1, 2, 3])
+        {
+            Console.WriteLine("리스트 패턴: 1, 2, 3 배열과 일치");
+        }
+
+        // SequenceEqual을 활용한 비교
+        if (numbers.SequenceEqual(new int[] { 1, 2, 3 }))
+        {
+            Console.WriteLine("SequenceEqual: 1, 2, 3 배열과 일치");
+        }
+    }
+}
