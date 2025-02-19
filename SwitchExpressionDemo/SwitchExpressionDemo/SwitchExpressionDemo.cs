21a35efe9366c9e454d9da884a97a8e4c67fee1a
Park YongJune <redplus@redplus.net>
2025-02-20 06:12:14 +0900

    SwitchExpressionDemo

---
+++ SwitchExpressionDemo/SwitchExpressionDemo/SwitchExpressionDemo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class SwitchExpressionDemo
+{
+    static void Main()
+    {
+        object[] testValues = { 42, 3.14, "Hello", true, null };
+
+        foreach (var obj in testValues)
+        {
+            string result = GetTypeDescription(obj);
+            Console.WriteLine(result);
+        }
+    }
+
+    static string GetTypeDescription(object obj) => obj switch
+    {
+        int i => $"정수 {i}",
+        double d => $"실수 {d}",
+        string s => $"문자열 \"{s}\"",
+        bool b => $"불리언 {b}",
+        null => "널 값",
+        _ => "알 수 없는 형식"
+    };
+}
