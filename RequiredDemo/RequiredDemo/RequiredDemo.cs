5201a2333a311fe1dc18bd6bda21b2561be8013a
Park YongJune <redplus@redplus.net>
2025-03-21 13:36:21 +0900

    .

--- RequiredDemo/RequiredDemo/RequiredDemo.cs
+++ RequiredDemo/RequiredDemo/RequiredDemo.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-public class PersonRequired
+﻿public class PersonRequired
 {
     public required string Name { get; set; } // 필수 속성
     public required int Age { get; set; } // 필수 속성
@@ -12,7 +10,7 @@ class RequiredDemo
     static void Main()
     {
         // 필수 속성을 포함한 올바른 개체 초기화
-        var person = new PersonRequired { Name = "홍길동", Age = 30 };
+        var person = new PersonRequired { Name = "홍길동", Age = 21 };
         Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
 
         // 필수 속성이 누락되면 컴파일 오류가 발생함
