df8704ce39e7b52bc90f17abd60537ae2b4cf8a0
Park YongJune <redplus@redplus.net>
2025-04-27 23:19:22 +0900

    ThreadPractice

---
+++ ThreadPractice/ThreadPractice/ThreadPractice.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+class ThreadPractice
+{
+    private static void Ide()
+    {
+        Thread.Sleep(3000); //3초 딜레이
+        Console.WriteLine("[3] IDE : Visual Studio");
+    }
+    private static void Sql()
+    {
+        Thread.Sleep(3000); //3초 딜레이
+        Console.WriteLine("[2] DBMS : SQL Server");
+    }
+    private static void Win()
+    {
+        Thread.Sleep(3000); //3초 딜레이
+        Console.WriteLine("[1] OS : Windows Server");
+    }
+    static void Main()
+    {
+        //① 스레드
+        ThreadStart ts1 = new ThreadStart(Win);
+        ThreadStart ts2 = new ThreadStart(Sql);
+        Thread t1 = new Thread(ts1);
+        var t2 = new Thread(ts2);
+        var t3 = new Thread(new ThreadStart(Ide))
+        {
+            Priority = ThreadPriority.Highest //우선순위 높게
+        };
+        t1.Start();
+        t2.Start();
+        t3.Start();
+        //② 프로세스
+        Process.Start("Notepad.exe"); //메모장 실행
+    }
+}
