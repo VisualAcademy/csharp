760b9d6961e177393132ebf83d4e6490b6350b72
Park YongJune <redplus@redplus.net>
2025-06-13 11:36:42 +0900

    Base64DecodingExample

---
+++ Base64DecodingExample/Base64DecodingExample/Base64DecodingExample.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+class Base64DecodingExample
+{
+    static void Main()
+    {
+        string base64Encoded = "MTIzNDU2";
+
+        // Base64 문자열을 바이트 배열로 디코딩한 후 UTF-8 문자열로 변환
+        string decoded = Encoding.UTF8.GetString(
+            Convert.FromBase64String(base64Encoded));
+
+        Console.WriteLine($"디코딩된 문자열: {decoded}");
+    }
+}
